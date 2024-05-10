1821eb3d90671b3bc56d085bf480debe9595083b
willcodinggod white <walidwhite@live.co.uk>
2024-05-10 09:00:27 +0000

    added division

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,8 +37,8 @@ function runGame(gameType) {
 
         displaySubtractQuestion(num1, num2);
 
-    } else if (gameType === "divide") {
-        displayDivideQuestion(num1,num2); 
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1,num2); 
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -114,9 +114,9 @@ function displayMultiplyQuestion(operand1, operand2) {
     
 }
 
-function displaydevisionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "/";
-    
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
